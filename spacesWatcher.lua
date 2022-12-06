dd0478e3176307848d2e64ebe1d84c555e19925e
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2022-12-06 15:49:30 +0800

    refactor: 优化代码

--- spacesWatcher.lua
+++
@@ -1,9 +0,0 @@
-local im = require('im')
-
-function spacesWatcherHandler()
-  im.english()
-end
-
-local spacesWatcher = hs.spaces.watcher.new(spacesWatcherHandler)
-
-return spacesWatcher
---
+++ watcher.lua
@@ -0,0 +1,40 @@
+local im = require('im')
+
+
+local function toggleIM(appName)
+	if (appName == 'Code') then
+		im.english()
+	elseif (appName == '阿里钉') then
+		im.chinese()
+	elseif (appName == '阿里语雀') then
+		im.chinese()
+	end
+end
+
+
+function applicationWatcher(appName, eventType, appObject)
+	if (eventType == hs.application.watcher.activated) then
+		toggleIM(appName)
+	end
+end
+
+
+local appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
+function spacesWatcherHandler()
+
+	appWatcher:stop()
+
+	local window = hs.window.focusedWindow()
+	if (window) then
+		local appName = window:application():name()
+		print(appName)
+		toggleIM(appName)
+	end
+
+	appWatcher:start()
+end
+
+local spacesWatcher = hs.spaces.watcher.new(spacesWatcherHandler)
+return spacesWatcher

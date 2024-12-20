04595af24f151282b21b84bd49c154758c91ad7d
MoYuM <hanbaochang12138@gmail.com>
2024-12-20 10:35:34 +0800

    feat

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 hs.loadSpoon("InputSourceSwitch")
+hs.loadSpoon("InputMethodIndicator")
 
 spoon.InputSourceSwitch:setApplications({
     ["微信"] = "Pinyin - Simplified",
     ["飞书"] = "Pinyin - Simplified",
     ["iTerm2"] = "ABC",
     ["Code"] = "ABC"
+})
+
+spoon.InputMethodIndicator:start({
+    mode = "onChange", -- the mode of the indicator
 })
 
 spoon.InputSourceSwitch:start()
